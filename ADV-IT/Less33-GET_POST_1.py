a7b3e7e16c9eb79158d31d9a8561f53270992396
Zakhar Eliseev <eliseev.zv@gmail.com>
2021-09-07 12:26:29 +0500

    update1

---
+++ ADV-IT/Less33-GET_POST_1.py
@@ -0,0 +1,18 @@
+from urllib import request
+import sys
+'''
+myURL = 'https://www.astahov.net'
+otvet = request.urlopen(myURL)
+
+mytext1 = otvet.readlines()
+mytext2 = otvet.read()
+
+print(otvet)
+print(mytext2)
+for line in mytext1:
+    print(line)
+
+'''
+
+myYrl = 'http://www.google.com/search?'
+value = {'q':'ANDESSA Soft'}
